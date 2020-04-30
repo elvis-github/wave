aca948859b6e8d31eb3e3d69f5ce630314738b69
Elvis Bui <elvisbui.ed@gmail.com>
2020-04-30 14:17:02 -0400

    Add fire and create 3x3 grid

--- js/index.js
+++ js/index.js
@@ -1,14 +1,25 @@
 function loadFiles() {
     var folder = "static/images";
-
+    var row = 0;
     $.ajax({
         url: folder,
         success: function (data) {
             $(data).find("a").attr("href", function (i, val) {
                 if (val.match(/\.(jpe?g|png|gif)$/)) {
-                    $("#main").append("<img src='" + val + "'>");
+                    if (row % 3 == 0) {
+                        row = 0;
+                    }
+                    if (row == 0) {
+                        $("#main").append($("<div class='row'></div>"));
+                    }
+                    row++;
+                    $(".row").last().append("<img src='" + val + "' class='col-4 p-5 img-fluid'>");
                 }
             });
         }
     });
+}
+
+function adjustImage() {
+
 }
